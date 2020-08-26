1e8f9b38f0f13daeb0fb11fe01f1049b43a6b0ee
radinreth <radin.reth@gmail.com>
2020-08-26 21:55:34 +0700

    update Car definition

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,7 @@
 class Car
   attr_accessor :plate_number, :colour
 
-  def initialize(plate_number:, colour:)
+  def initialize(plate_number, colour)
     @plate_number = plate_number
     @colour = colour
   end
--- spec/lib/car_spec.rb
+++ spec/lib/car_spec.rb
@@ -1,7 +1,7 @@
 require 'car'
 
 RSpec.describe Car do
-  let(:car) { Car.new(plate_number: 'ABC-1234', colour: 'White') }
+  let(:car) { Car.new('ABC-1234', 'White') }
 
   specify { expect(car.plate_number).to eq('ABC-1234') }
   specify { expect(car.colour).to eq('White') }
